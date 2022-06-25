55ba60b9ec7514c263cbc07f96f3f749e78f1d6d
ckntucker <ckntucker@gmail.com>
2022-06-24 15:46:06 -1000

    html need to add content css is pretty much done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Portfolio
+.top-nav-grid{
+    
+}
--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('success')
+//Toggles the nav to bring out to a hover
+const showMenu=(toggleId, navId) =>{
+    const toggle= document.getElementById(toggleId),
+    nav= document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('toggle-nav','nav-menu')
+
+const navLink=document.querySelectorAll('.nav-link')
+
+//Removes active link then places active onto the clicked on link
+function linkRemove(){
+    navLink.forEach(n=> n.classList.remove('active'))
+    this.classList.add('active')
+//Removes the nav menu after clicking navlink
+    const navMenu=document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n=> n.addEventListener('click', linkRemove))
+    
+
